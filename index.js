fd2c0c7f0fa47aa493c242914f17baf84cbe76b4
Marvin Laucher <marv070@gmail.com>
2017-05-23 15:14:45 -0400

    360i pushes uploaded files to redshift through lambda when uploaded to s3

--- index.js
+++ index.js
@@ -1,24 +1,19 @@
 //raw connection 
 var redshiftClient = require('./redshift.js');
  
+function test(event) {
+    console.log(event.Records[0].s3.object.key,'event is here !!!!');
 redshiftClient.connect(function(err){
   if(err) throw err;
   else{
-    // redshiftClient.query("INSERT INTO public.part(p_partkey, p_name, p_mfgr, p_category, p_brand1, p_color, p_type, p_size, p_container)VALUES(66, 'junk', 'Dodge', 'elec', 'bose', 'pink', 'speaker', 0, 'jumbo');",{raw: true}, function(err, data){
-    //   if(err) throw err;
-    //   else{
-    //     console.log(data);
-    //     redshiftClient.close();
-    //   }
-    // });
-
-    var uploaded_file = 'part-csv.tbl-002',
-      key_id = process.env.aws_access_key_id
+   
+    var uploaded_file = event.Records[0].s3.object.key,
+      key_id = process.env.aws_access_key_id;
       console.log(key_id,"key id here");
-      access_key = process.env.aws_secret_access_key
-      console.log(access_key, 'access key here')
-      console.log("copy part from 's3://test-360i/load/"+ uploaded_file +"' credentials 'aws_access_key_id=" + key_id + "aws_secret_access_key=" + access_key +" ' csv;")
-    redshiftClient.query("copy part from 's3://test-360i/load/"+ uploaded_file +"' credentials 'aws_access_key_id=" + key_id + ";aws_secret_access_key=" + access_key +"' csv;",{raw: true}, function(err, data){
+      access_key = process.env.aws_secret_access_key;
+      console.log(access_key, 'access key here');
+      console.log("copy part from 's3://test-360i/"+ uploaded_file +"' credentials 'aws_access_key_id=" + key_id + "aws_secret_access_key=" + access_key +" ' csv;");
+    redshiftClient.query("copy part from 's3://test-360i/"+ uploaded_file +"' credentials 'aws_access_key_id=" + key_id + ";aws_secret_access_key=" + access_key +"' csv;",{raw: true}, function(err, data){
       if(err) throw err;
       else{
         console.log(data);
@@ -28,8 +23,12 @@ redshiftClient.connect(function(err){
     //instead of callbacks you can also use promises to get the data 
   }
 });
+}
 
-// exports.handler = (event, context, callback) => {
-//     // TODO implement
-//     callback(null, 'Hello from Lambda');
-// };
+exports.handler = (event, context, callback) => {
+    console.log(event,"export event here !!!!!!!");
+    // TODO implement
+    test(event);
+    console.log("test");
+    callback(null, 'Hello from Lambda');
+};
